4b9e41416b80d6dd70c76964b40af458f5265903
Navjit Bath <nsbath@csuchico.edu>
2023-02-13 12:42:06 -0800

    delete unused files

---
+++ Makefile
@@ -0,0 +1,4 @@
+main: main.cpp
+	
+	g++ -c main.cpp -std=c++11 main.cpp
+	g++ -o main main.o -std=c++11
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+
+    cout<< "hello mate bro\n";
+    return 0;
+}
